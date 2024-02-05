e18ad55c4f97f3e86a2f095f15d0d3c1f2f4c21a
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-02-05 18:31:18 +0530

    added new mwthod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        CourseRegistration registration = new CourseRegistration();
+
+        System.out.print("Enter course name: ");
+        String courseName = scanner.nextLine();
+
+        System.out.print("Enter lecturer name: ");
+        String lectureName = scanner.nextLine();
+
+        registration.registerCourse(courseName, lectureName);
+
+        scanner.close();
     }
 }
